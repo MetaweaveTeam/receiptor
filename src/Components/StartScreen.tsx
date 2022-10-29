930144937e7c7055f5751c60b515537c4f569f2a
bidetaggle <bidetaggle@gmail.com>
2022-10-29 02:56:53 +0100

    add disconnect

--- src/Components/StartScreen.tsx
+++ src/Components/StartScreen.tsx
@@ -1,7 +1,7 @@
-import { Text, Dropdown, Input, Image, Grid, Checkbox, Spacer, Container, FormElement } from '@nextui-org/react';
+import { Text, Dropdown, Input, Image, Grid, Checkbox, Spacer, Container, FormElement, Button } from '@nextui-org/react';
 import React, { useRef, useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { useConnectModal } from '@web3modal/react';
+import { useConnectModal, useAccount, useDisconnect } from '@web3modal/react';
 import { ethers } from 'ethers';
 import './StartScreen.css';
 
@@ -15,6 +15,8 @@ export default function StartScreen() {
   const [formIsValid, setFormIsValid] = useState(false);
   const formStatus = useRef({txid: false, tac: false})
   const walletConnect = useConnectModal()
+  const walletAccount = useAccount()
+  const disconnect = useDisconnect()
 
   const updateFormStatus = () => setFormIsValid(formStatus.current.txid && formStatus.current.tac)
 
@@ -82,17 +84,24 @@ export default function StartScreen() {
         </Checkbox>
       </Grid>
       <Grid>
-        <button
-          disabled={!formIsValid}
-          onClick={walletConnect.open}
-          style={!formIsValid ? {opacity: 0.2, cursor: 'not-allowed'} : {}}
-          className={`wallet-connect-button ${walletConnect.isOpen && 'wallet-connect-button-connecting'}`}
-        >
-          <svg width="28" height="20" viewBox="0 0 28 20">
-            <path fill={walletConnect.isOpen ? 'rgba(81,109,251,1)' : '#ffffff'} d="M7.386 6.482c3.653-3.576 9.575-3.576 13.228 0l.44.43a.451.451 0 0 1 0 .648L19.55 9.033a.237.237 0 0 1-.33 0l-.606-.592c-2.548-2.496-6.68-2.496-9.228 0l-.648.634a.237.237 0 0 1-.33 0L6.902 7.602a.451.451 0 0 1 0-.647l.483-.473Zm16.338 3.046 1.339 1.31a.451.451 0 0 1 0 .648l-6.035 5.909a.475.475 0 0 1-.662 0L14.083 13.2a.119.119 0 0 0-.166 0l-4.283 4.194a.475.475 0 0 1-.662 0l-6.035-5.91a.451.451 0 0 1 0-.647l1.338-1.31a.475.475 0 0 1 .662 0l4.283 4.194c.046.044.12.044.166 0l4.283-4.194a.475.475 0 0 1 .662 0l4.283 4.194c.046.044.12.044.166 0l4.283-4.194a.475.475 0 0 1 .662 0Z"></path>
-          </svg>
-          {walletConnect.isOpen ? 'Connecting...' : 'Connect Wallet'}
-        </button>
+        {walletAccount.account.isConnected
+        ? <>
+            {walletAccount.account.address}
+            <Button color="warning" onClick={disconnect}>disconnect</Button>
+            <Button disabled={!formIsValid} color="secondary">continue</Button>
+          </>
+        : <button
+            disabled={!formIsValid}
+            onClick={walletConnect.open}
+            style={!formIsValid ? {opacity: 0.2, cursor: 'not-allowed'} : {}}
+            className={`wallet-connect-button ${walletConnect.isOpen && 'wallet-connect-button-connecting'}`}
+          >
+            <svg width="28" height="20" viewBox="0 0 28 20">
+              <path fill={walletConnect.isOpen ? 'rgba(81,109,251,1)' : '#ffffff'} d="M7.386 6.482c3.653-3.576 9.575-3.576 13.228 0l.44.43a.451.451 0 0 1 0 .648L19.55 9.033a.237.237 0 0 1-.33 0l-.606-.592c-2.548-2.496-6.68-2.496-9.228 0l-.648.634a.237.237 0 0 1-.33 0L6.902 7.602a.451.451 0 0 1 0-.647l.483-.473Zm16.338 3.046 1.339 1.31a.451.451 0 0 1 0 .648l-6.035 5.909a.475.475 0 0 1-.662 0L14.083 13.2a.119.119 0 0 0-.166 0l-4.283 4.194a.475.475 0 0 1-.662 0l-6.035-5.91a.451.451 0 0 1 0-.647l1.338-1.31a.475.475 0 0 1 .662 0l4.283 4.194c.046.044.12.044.166 0l4.283-4.194a.475.475 0 0 1 .662 0l4.283 4.194c.046.044.12.044.166 0l4.283-4.194a.475.475 0 0 1 .662 0Z"></path>
+            </svg>
+            {walletConnect.isOpen ? 'Connecting...' : 'Connect Wallet'}
+          </button>
+        }
       </Grid>
     </Grid.Container>
     <Spacer y={0.5} />
